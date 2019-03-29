e729d7e86d553f8b0af9ef8b484086cfc3a47566
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-03-29 15:19:35 +0200

    playing POST with DTO

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
     @Get()
     findAll(): string {
         return 'Get All Items'
+    }
+
+    @Post()
+    createItem(@Body() createItemDto: CreateItemDto): string {
+        return `Created ${createItemDto.name}!`
     }
 
 }
